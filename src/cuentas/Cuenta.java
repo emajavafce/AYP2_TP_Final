31f1562adb6486d517e1bef615fbfb0b0528cec5
emajavafce <emafaillace2018@gmail.com>
2025-11-07 03:27:45 -0300

    Se agregaron metodos y atributos a la clase Cuenta

--- src/cuentas/Cuenta.java
+++ src/cuentas/Cuenta.java
@@ -1,4 +1,16 @@
 package cuentas;
 
 public class Cuenta {
+
+    private double saldo;
+    private String tipo;
+    private String alias;
+
+    public void aumentarSaldo(double cantidad) {
+
+    }
+
+    public void disminuirSaldo(double cantidad) {
+
+    }
 }
